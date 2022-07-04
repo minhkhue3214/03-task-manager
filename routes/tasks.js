ae7ad55ba5e4e28247bf39dd986ebb8f5f70788d
minh khue <minhkhue3214@gmail.com>
2022-07-04 09:02:50 +0700

    4/7

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router();
 
-router.route('/').get((req,res)=>{
-    res.send('all items')
-})
+const {getAllTasks,createTask,getTask,updateTask,deleteTask} = require('../controllers/tasks')
 
-module.export = router
+router.route('/').get(getAllTasks).post(createTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+
+module.exports = router
